3248b4023128109d4fbbb2cfa6bf89823d8b7bca
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-01-11 08:56:31 -0700

    player pojo

--- Sisters before Misters/src/Player.java
+++ Sisters before Misters/src/Player.java
@@ -2,9 +2,47 @@
 public class Player
 	{
 
-		public static void main(String[] args)
+		private String character;
+		private int balance;
+		private int place;
+		
+		public Player(String c, int b, int p)
 			{
-
+				character = c;
+				balance = b;
+				place = p;
 			}
+
+		public String getCharacter()
+			{
+				return character;
+			}
+
+		public void setCharacter(String character)
+			{
+				this.character = character;
+			}
+
+		public int getBalance()
+			{
+				return balance;
+			}
+
+		public void setBalance(int balance)
+			{
+				this.balance = balance;
+			}
+
+		public int getPlace()
+			{
+				return place;
+			}
+
+		public void setPlace(int place)
+			{
+				this.place = place;
+			}
+		
+		
 
 	}
