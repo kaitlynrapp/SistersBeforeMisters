aa79c25c0141fa5f818abbe225a48aa4540f2ee1
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-01-13 08:32:08 -0700

    chest runner done

--- Sisters before Misters/src/ChestRunner.java
+++ Sisters before Misters/src/ChestRunner.java
@@ -7,9 +7,16 @@ public class ChestRunner
 				ArrayList<Chest> cards = new ArrayList<Chest>();
 				
 				cards.add(new Chest("You went to the movies pay $50", -50));
-				//cards.add(new Chance("Collect $200!", 0, 200));
-				//cards.add(new Chance("You Lost $200", 0, -200));
-				//cards.add(new Chance("Go forward three spaces", 3, 0));
+				cards.add(new Chest("You stole snacks pay $100", -100));
+				cards.add(new Chest("You bought Disney Stock earn $200", +200));
+				cards.add(new Chest("You went to the disney store pay $400", -400));
+				cards.add(new Chest("You went to disneyland pay $300", -300));
+				cards.add(new Chest("You stole mickey mouse pay $600", -600));
+				cards.add(new Chest("You bought Frozen pay $50", -50));
+				cards.add(new Chest("You got a subscription to Disney+, pay $100", -100));
+				cards.add(new Chest("You sold disney merch earn $100", +100));
+
+				
 				
 			}
 	}
