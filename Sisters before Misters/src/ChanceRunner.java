75fcaed484cff2b3e3b755416c285c449d753eb9
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-01-13 08:42:06 -0700

    done with chance runner

--- Sisters before Misters/src/ChanceRunner.java
+++ Sisters before Misters/src/ChanceRunner.java
@@ -5,7 +5,7 @@ public class ChanceRunner
 		public static void Chance()
 			{
 				ArrayList<Chance> cards = new ArrayList<Chance>();
-				
+				//done
 				cards.add(new Chance("Go back three spaces", -3, 0));
 				cards.add(new Chance("Collect $200!", 0, 200));
 				cards.add(new Chance("You Lost $200", 0, -200));
