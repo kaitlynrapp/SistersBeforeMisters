d56146f71977fbd4dc2c49d8ffe72a12d28d24c8
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-01-13 08:41:11 -0700

    finished chance runner

--- Sisters before Misters/src/ChanceRunner.java
+++ Sisters before Misters/src/ChanceRunner.java
@@ -10,6 +10,12 @@ public class ChanceRunner
 				cards.add(new Chance("Collect $200!", 0, 200));
 				cards.add(new Chance("You Lost $200", 0, -200));
 				cards.add(new Chance("Go forward three spaces", 3, 0));
+				cards.add(new Chance("Collect $300 and move forward two spaces", 2, 300));
+				cards.add(new Chance("Lose $100 and move backward two spaces", 2, -100));
+				cards.add(new Chance("Collect $100 and move forward one space", 1, 100));
+				cards.add(new Chance("Move back two spaces", -2, 0));
+				cards.add(new Chance("Lose $600", 0, -600));
+				cards.add(new Chance("Move foward ten spaces", 10,0));
 				
 			}
 
