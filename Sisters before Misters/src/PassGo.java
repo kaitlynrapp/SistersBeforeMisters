2e7e70a464feebb6210c36bee23c3ef0bcc7308f
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2022-01-18 11:57:48 -0700

    balance done. The player has a new class in it too

--- Sisters before Misters/src/PassGo.java
+++ Sisters before Misters/src/PassGo.java
@@ -3,7 +3,7 @@ public class PassGo
 	{
 		public static void PassGo()
 			{
-			MonopolyRunner.player1.getBalance() = MonopolyRunner.player1.getBalance() + 200;
+			MonopolyRunner.player1.addToBalance(200);
 			System.out.println("YAY! You finished a movie! Collect $200");
 			System.out.println("Your new balance is: " + MonopolyRunner.player1.getBalance() + " ");
 			}
